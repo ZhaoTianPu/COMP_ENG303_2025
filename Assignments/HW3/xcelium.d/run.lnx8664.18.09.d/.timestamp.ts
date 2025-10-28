f6aa6b5cc02e2098514880aacd33aa46a5209387
Tianpu Zhao <pacosynthesis@gmail.com>
2025-10-28 18:38:59 -0500

    Upload simulation results

---
+++ Assignments/HW3/xcelium.d/run.lnx8664.18.09.d/.timestamp.ts
@@ -0,0 +1,4 @@
+1761694368 /home/tzc3193/CE303/COMP_ENG303_2025/Assignments/HW3/Q3_multiplier.v
+1761694368 /home/tzc3193/CE303/COMP_ENG303_2025/Assignments/HW3/Q4_barrel.v
+1761694368 /home/tzc3193/CE303/COMP_ENG303_2025/Assignments/HW3/Q4_tb_barrel.v
+1761694368 /home/tzc3193/CE303/COMP_ENG303_2025/Assignments/HW3/Q3_tb_multiplier.v
