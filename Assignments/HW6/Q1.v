8c38aa6f0bb2a701cf30cf6f9cba8fa78ce58de1
Tianpu Zhao <pacosynthesis@gmail.com>
2025-12-02 18:12:21 -0600

    Update HW6

---
+++ Assignments/HW6/Q1.v
@@ -0,0 +1,34 @@
+`timescale 1ns/1ps
+module Vredge (
+    input  wire clk,
+    input  wire rst,
+    input  wire X,
+    output reg  EDGE
+);
+
+    localparam A_00 = 2'b00; // state A: two 0s
+    localparam B_01 = 2'b01; // state B: one 0, one 1
+    localparam C_10 = 2'b10; // state C: one 1, one 0
+    localparam D_11 = 2'b11; // state D: two 1s
+
+    reg [1:0] state;
+
+    assign EDGE = (state == B_01) || (state == C_10);
+
+    always @(posedge clk or posedge rst) begin
+        if (rst)
+            state <= A_00;
+        else
+            case (state)
+                A_00:
+                    state <= X ? B_01 : A_00;
+                B_01:
+                    state <= X ? D_11 : C_10;
+                C_10:
+                    state <= X ? B_01 : A_00;
+                D_11:
+                    state <= X ? D_11 : C_10;
+            endcase
+        end
+
+endmodule
---
+++ Assignments/HW6/Q5_tb.v
@@ -0,0 +1,14 @@
+module tb_counters;
+    reg CLK=0, CLR=1;
+    wire [7:0] S1, S2;
+
+    Vr3bitctrdec     u1(CLK,CLR,S1);
+    Vr3bitctrdecreg  u2(CLK,CLR,S2);
+
+    always #5 CLK = ~CLK;
+
+    initial begin
+        #12 CLR=0;
+        #300 $finish;
+    end
+endmodule
